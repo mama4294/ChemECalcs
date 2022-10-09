f3ce5cee541eb487116b47c28c718eb6f12dab12
mama4294 <matthew.malone@colorado.edu>
2022-10-09 08:39:30 -0600

    adds label on solve for selector

--- chem_e_calc/src/pages/geometry.tsx
+++ chem_e_calc/src/pages/geometry.tsx
@@ -384,19 +384,24 @@ const SolveForDropdown = ({ options, onChange }: SolveForProps) => {
   const selectedValueId = options.find(option => option.selected === true)?.id
 
   return (
-    <select
-      className="select input-bordered w-full bg-base-200 text-base-content"
-      value={selectedValueId}
-      onChange={e => onChange(Number(e.target.value))}
-    >
-      {options.map(option => {
-        if (option.solveable)
-          return (
-            <option key={option.id} value={option.id}>
-              {option.label}
-            </option>
-          )
-      })}
-    </select>
+    <div className="form-control w-full">
+      <label className="label">
+        <span className="label-text">Solve for</span>
+      </label>
+      <select
+        className="select input-bordered w-full bg-base-200 text-base-content"
+        value={selectedValueId}
+        onChange={e => onChange(Number(e.target.value))}
+      >
+        {options.map(option => {
+          if (option.solveable)
+            return (
+              <option key={option.id} value={option.id}>
+                {option.label}
+              </option>
+            )
+        })}
+      </select>
+    </div>
   )
 }
