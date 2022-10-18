f0463af8032e5df7a4d4feee1e695784ee50213f
mama4294 <matthew.malone@colorado.edu>
2022-10-18 07:35:17 -0600

    updates footer

--- chem_e_calc/src/components/Layout/Footer.tsx
+++ chem_e_calc/src/components/Layout/Footer.tsx
@@ -1,7 +1,8 @@
 const Footer = () => {
   return (
-    <footer className="footer flex h-12 items-center justify-end bg-neutral text-neutral-content ">
-      <div className="text-ghost mx-4">Matthew Malone 2022</div>
+    <footer className="footer flex h-12 items-center justify-between bg-neutral text-neutral-content ">
+      <div className="text-ghost mx-4">Matthew Malone</div>
+      <div className="text-ghost mx-4">Copyright © 2022 - All right reserved</div>
     </footer>
   )
 }
