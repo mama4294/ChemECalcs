79270f01945dd1d6ee1a7508bed8c93e7b9d409b
mama4294 <matthew.malone@colorado.edu>
2023-11-10 08:12:55 -0700

    connects theme to logo colors

--- chem_e_calc/src/components/Layout/Logo.tsx
+++ chem_e_calc/src/components/Layout/Logo.tsx
@@ -1,34 +1,8 @@
-export const ChemECalcsLogo = ({
-  lightMode = false,
-  height,
-  width,
-}: {
-  lightMode?: boolean
-  height: number
-  width: number
-}) => {
-  const accentColor = '#00899f'
-  const logoColor = '#00899f'
+export const ChemECalcsLogo = ({ height, width }: { height: number; width: number }) => {
+  const accentColor = 'hsl(var(--p))'
+  const logoColor = 'hsl(var(--p))'
 
   return (
-    // <svg xmlns="http://www.w3.org/2000/svg" width={48} height={48}>
-    //   {/* <linearGradient
-    //     id="a"
-    //     x1={10.341}
-    //     x2={40.798}
-    //     y1={8.312}
-    //     y2={38.769}
-    //     gradientUnits="userSpaceOnUse"
-    //   >
-    //     <stop offset={0} stopColor="#2aa4f4" />
-    //     <stop offset={1} stopColor="#007ad9" />
-    //   </linearGradient> */}
-    //   <path
-    //     fill="url(#a)"
-    //     d="M46.105 ..."
-    //   />
-    // </svg>
-
     <svg
       version="1.1"
       id="Layer_1"
