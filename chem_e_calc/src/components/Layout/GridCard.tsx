ba621aeea8f9c3eefd0334c5ada0edd95d2150e5
mama4294 <matthew.malone@colorado.edu>
2023-11-10 18:21:02 -0700

    adds neon glow around gridcards

--- chem_e_calc/src/components/Layout/GridCard.tsx
+++ chem_e_calc/src/components/Layout/GridCard.tsx
@@ -29,26 +29,18 @@ export const GridCard = ({ name, children, description, link, span }: GridCardPr
   return (
     <Link href={link}>
       <div
-        className={`group grid cursor-pointer rounded-xl border border-primary-content bg-base-100/50 p-6 text-left text-base-content duration-500 hover:border-primary hover:bg-base-100/70 ${spanClass}`} //motion-safe:hover:scale-105
+        className={`relative group grid cursor-pointer duration-500 hover:scale-105    ${spanClass}`} //motion-safe:hover:scale-105
       >
-        <h3 className="mb-3 flex items-center text-2xl font-semibold tracking-tight">
-          {name}
-          <span className="ml-1 inline-block transition-transform group-hover:translate-x-1 group-hover:text-primary motion-reduce:transform-none">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="h-6 w-6"
-            >
-              <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-            </svg>
-          </span>
-        </h3>
+        <div className='absolute -inset-0.5 bg-secondary blur rounded-xl opacity-75 group-hover:opacity-100 group-hover:-inset-1 duration-500'></div>
+        <div className='relative text-left bg-neutral text-neutral-content p-6 rounded-xl'>
+          <h3 className="mb-3 flex items-center text-2xl font-semibold tracking-tight">
+            {name}
+          </h3>
         <p className="text-sm opacity-75">{description}</p>
         {children && <div className="flex justify-center">{children}</div>}
       </div>
+      </div>
+
     </Link>
   )
 }
